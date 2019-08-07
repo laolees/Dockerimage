ddb2de14c5d47e98dfd5ed0fcac18700aa81204b
lijiyi <lijiyi_321@163.com>
2019-08-07 16:35:03 +0800

    fixed bug

--- push-images.sh
+++ push-images.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-sudo tee /etc/docker/daemon.json <<-'EOF'
-{
-"insecure-registries": ["114.115.138.52:8443"]
-}
-EOF
-
-systemctl reload docker
-
 for imagepath in $(cat .imagepath.txt)
 do
 imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
 docker pull $imagepath
 
 # push到私有仓库
-docker tag $imagepath 114.115.138.52:8443/admin/$imagename
+docker tag $imagepath 114.115.138.52/admin/$imagename
 docker push 114.115.138.52:8443/admin/$imagename
 
 # push到dockerhub
