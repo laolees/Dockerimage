fd7d804bde47cc602fad1236ccc2a8463ebe0bc1
lijiyi <lijiyi_321@163.com>
2019-08-07 14:16:24 +0800

    first commit

---
+++ push-images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for imagepath in $(cat .imagepath.txt)
+do
+imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
+docker pull $imagepath
+
+# push到私有仓库
+docker tag $imagepath 114.115.138.52:8443/admin/$imagename
+docker push 114.115.138.52:8443/admin/$imagename
+
+# push到dockerhub
+docker tag $imagepath 599680041/$imagename
+docker push 599680041/$imagename
+done
