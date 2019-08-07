73bdb71c8d165912e6d3a159b3b78a4f8ca8c781
lijiyi <lijiyi_321@163.com>
2019-08-07 17:33:11 +0800

    fixed bug

--- push-images.sh
+++ push-images.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for imagepath in $(cat .imagepath.txt)
+for imagepath in $(cat ./imagepath.txt)
 do
 imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
 docker pull $imagepath
 
 # push到私有仓库
 docker tag $imagepath 114.115.138.52/admin/$imagename
-docker push 114.115.138.52:8443/admin/$imagename
+docker push 114.115.138.52/admin/$imagename
 
 # push到dockerhub
 docker tag $imagepath 599680041/$imagename
