a1289dad623fe96d51166b970f0f0886d8fcbd09
lijiyi <lijiyi_321@163.com>
2019-08-08 15:32:51 +0800

    fixed bug

--- push-images.sh
+++ push-images.sh
@@ -6,8 +6,8 @@ imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
 docker pull $imagepath
 
 # push到私有仓库
-docker tag $imagepath 114.115.138.52/admin/$imagename
-docker push 114.115.138.52/admin/$imagename
+docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/k8mirror/$imagename
+docker push registry.cn-hangzhou.aliyuncs.com/k8mirror/$imagename
 
 # push到dockerhub
 docker tag $imagepath 599680041/$imagename
